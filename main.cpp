8ef90e523205d804c7f46b3b06051b3804c82ae6
1080heidi <heidideleon415@gmail.com>
2025-11-09 09:08:55 -0800

    unit test drivers

--- main.cpp
+++ main.cpp
@@ -75,36 +75,5 @@ using namespace std;
 
 
 
-
-// insert those values into the map:
-// key = particle name
-// value = array of three lists for x, y, z coordinates
-// close the file
-
-// define a function to simulate motion in the vector field
-// parameters: map of particles, number of time steps, time interval (Δt)
-// for each time step:
-// iterate through each particle in the map
-// retrieve the last x, y, z position
-// compute the vector field F(x, y, z) = (y, -x, z)
-// update each position using:
-// x_new = x + Fx * Δt
-// y_new = y + Fy * Δt
-// z_new = z + Fz * Δt
-// print the new coordinates for this particle
-// end particle loop
- // end time-step loop
-
-// define a function to display current particle positions
-// iterate through the map
-// print each particle’s most recent x, y, z coordinates
-
-// define the main function
-// declar a map of particles, each with an array of three lists for x, y, z coordinates
-// call the load function to read initial particle data from "field_data.txt"
-// call the simulation function to update positions over 25 time periods
-// display the final state of the system
-// end of main function
-
         
     
